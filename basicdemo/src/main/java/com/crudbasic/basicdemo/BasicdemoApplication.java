c20827a686a45b00829d8ca62ea5b006e3692ae2
narasimha <ynarasimha140@gmail.com>
2022-06-07 13:03:47 +0530

    first thing

---
+++ basicdemo/src/main/java/com/crudbasic/basicdemo/BasicdemoApplication.java
@@ -0,0 +1,13 @@
+package com.crudbasic.basicdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicdemoApplication.class, args);
+	}
+
+}
